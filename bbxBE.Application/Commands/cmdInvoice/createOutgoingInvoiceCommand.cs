fa8c99a3f500a9a5e467121bca20f7afd98db712
Györgyi Antal <antal_gyorgyi@freemail.hu>
2022-04-01 22:45:54 +0200

    jav

--- bbxBE.Application/Commands/cmdInvoice/createOutgoingInvoiceCommand.cs
+++ bbxBE.Application/Commands/cmdInvoice/createOutgoingInvoiceCommand.cs
@@ -1,4 +1,4 @@
-﻿using AutoMapper;
+using AutoMapper;
 using AutoMapper.Configuration.Conventions;
 using bbxBE.Application.BLL;
 using bbxBE.Application.Consts;
@@ -24,6 +24,10 @@ namespace bxBE.Application.Commands.cmdInvoice
 		[Description("Számlasor")]
 		public class InvoiceLine
 		{ 
+
+			[ColumnLabel("#")]
+			[Description("Sor száma")]
+			public short LineNumber { get; set; }
 
 			[ColumnLabel("Termékkód")]
 			[Description("Termékkód")]
@@ -53,7 +57,19 @@ namespace bxBE.Application.Commands.cmdInvoice
 			public decimal lineVatAmount { get; set; }
 
 		}
+/*
+		[Description("Számla áfánkénti összesítő")]
+		public class SummaryByVatRate 
+		{
+			[ColumnLabel("Áfakód")]
+			[Description("Áfakód")]
+			public string VatRateCode { get; set; }
 
+			[ColumnLabel("Áfa értéke")]
+			[Description("Áfa értéke a számla pénznemében")]
+			public decimal VatRateNetAmount { get; set; }
+		}
+*/
 		[ColumnLabel("Raktár")]
 		[Description("Raktár")]
 		public long WarehouseCode { get; set; }
