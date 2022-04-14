138c4b1dfaab4a522f9658bed27bb584d4242537
Györgyi Antal <antal_gyorgyi@freemail.hu>
2022-04-14 22:04:37 +0200

    x

---
+++ bbxBE.Application/Interfaces/ICacheService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bbxBE.Application.Interfaces
+{
+    public interface ICacheService
+    {
+        bool TryGet<T>(long ID, out T value);
+        T Set<T>(long ID, T value);
+        void Remove(long ID);
+    }
+}
