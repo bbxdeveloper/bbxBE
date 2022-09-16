35c8179b577171fdfbf3ac7623c853113097ed82
Györgyi Antal <antal.gyorgyi@medicover.hu>
2022-09-16 14:59:56 +0200

    II

--- bbxBE.Infrastructure.Persistence/Repositories/StockRepositoryAsync.cs
+++ bbxBE.Infrastructure.Persistence/Repositories/StockRepositoryAsync.cs
@@ -99,7 +99,7 @@ namespace bbxBE.Infrastructure.Persistence.Repositories
                     var latestStockCard = await _stockCardRepository.CreateStockCard(stock, invoice.InvoiceDeliveryDate,
                                 invoice.WarehouseID, invoiceLine.ProductID, invoice.UserID, invoiceLine.ID,
                                 (invoice.Incoming ? invoice.SupplierID : invoice.CustomerID),
-                                Common.Enums.enStockCardType.INVOICE,
+                                Common.Enums.enStockCardType.INV_DLV,
                                 invoiceLine.Quantity * (invoice.Incoming ? 1 : -1),
                                 invoiceLine.Quantity * (invoice.Incoming ? 1 : -1),
                                 0, invoiceLine.UnitPrice,
