40f39a6c6da3defebc02810fbb1a61ecafffd49a
agyorgyi01 <agyorgyi01@gmail.com>
2022-12-01 16:06:58 +0100

    Location OK!

---
+++ bbxBE.WebApi/Controllers/v1/LocationController.cs
@@ -0,0 +1,80 @@
+﻿using bbxBE.Application.Commands.cmdLocation;
+using bbxBE.Application.Commands.cmdUser;
+using bbxBE.Application.Interfaces.Queries;
+using bbxBE.Application.Queries.qLocation;
+using bbxBE.Application.Wrappers;
+using bbxBE.Domain.Entities;
+using bxBE.Application.Commands.cmdLocation;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace bbxBE.WebApi.Controllers.v1
+{
+    [ApiVersion("1.0")]
+ //   [Authorize]
+    public class LocationController : BaseApiController
+    {
+        private readonly IWebHostEnvironment _env;
+        private readonly IConfiguration _conf;
+        public LocationController( IWebHostEnvironment env, IConfiguration conf)
+        {
+            _env = env;
+            _conf = conf;
+        }
+
+
+        /// <summary>
+        /// GET: api/controller
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] GetLocation filter)
+        {
+            return Ok(await Mediator.Send(filter));
+        }
+
+        /// <summary>
+        /// GET: api/controller
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        [HttpGet("query")]
+        public async Task<IActionResult> Query([FromQuery] QueryLocation filter)
+        {
+            return Ok(await Mediator.Send(filter));
+        }
+
+        /// <summary>
+        /// POST api/controller
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateLocationCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+
+
+        // POST: USRController/Edit/5
+        [HttpPut]
+ //       [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(UpdateLocationCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+
+        // GET: USRController/Delete/5
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromQuery] DeleteLocationCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+    }
+}
