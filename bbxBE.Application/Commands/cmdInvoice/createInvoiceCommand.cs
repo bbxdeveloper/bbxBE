5fc3f308e9294346c930977ea4668ab157b0d716
gyorgyia <antal.gyorgyi@medicover.hu>
2023-04-21 08:51:40 +0200

    incoming az invoice get modelbe

--- bbxBE.Application/Commands/cmdInvoice/createInvoiceCommand.cs
+++ bbxBE.Application/Commands/cmdInvoice/createInvoiceCommand.cs
@@ -1,125 +1,116 @@
 ﻿using AutoMapper;
 using bbxBE.Application.BLL;
-using bbxBE.Common.Consts;
-using bbxBE.Common.Exceptions;
 using bbxBE.Application.Interfaces.Repositories;
 using bbxBE.Application.Wrappers;
 using bbxBE.Common.Attributes;
-using bbxBE.Common.Enums;
+using bbxBE.Common.ExpiringData;
 using bbxBE.Domain.Entities;
 using MediatR;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Configuration;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using static bbxBE.Common.NAV.NAV_enums;
-using bbxBE.Common.NAV;
-using Microsoft.EntityFrameworkCore.Storage;
-using Newtonsoft.Json.Linq;
-using bbxBE.Common.ExpiringData;
 
 namespace bxBE.Application.Commands.cmdInvoice
 {
-	public class CreateInvoiceCommand : IRequest<Response<Invoice>>
-	{
+    public class CreateInvoiceCommand : IRequest<Response<Invoice>>
+    {
 
-		[Description("Számlasor")]
-		public class InvoiceLine
-		{
+        [Description("Számlasor")]
+        public class InvoiceLine
+        {
 
-			[ColumnLabel("#")]
-			[Description("Sor száma")]
-			public short LineNumber { get; set; }
+            [ColumnLabel("#")]
+            [Description("Sor száma")]
+            public short LineNumber { get; set; }
 
-			[ColumnLabel("Termékkód")]
-			[Description("Termékkód")]
-			public string ProductCode { get; set; }
+            [ColumnLabel("Termékkód")]
+            [Description("Termékkód")]
+            public string ProductCode { get; set; }
 
-			[ColumnLabel("Áfakód")]
-			[Description("Áfakód")]
-			public string VatRateCode { get; set; }
+            [ColumnLabel("Áfakód")]
+            [Description("Áfakód")]
+            public string VatRateCode { get; set; }
 
-			[ColumnLabel("Mennyiség")]
-			[Description("Mennyiség")]
-			public decimal Quantity { get; set; }
+            [ColumnLabel("Mennyiség")]
+            [Description("Mennyiség")]
+            public decimal Quantity { get; set; }
 
-			[ColumnLabel("Me.e.")]
-			[Description("Mennyiségi egység kód")]
-			public string UnitOfMeasure { get; set; }
-			[ColumnLabel("Ár")]
-			[Description("Ár")]
-			public decimal UnitPrice { get; set; }
+            [ColumnLabel("Me.e.")]
+            [Description("Mennyiségi egység kód")]
+            public string UnitOfMeasure { get; set; }
+            [ColumnLabel("Ár")]
+            [Description("Ár")]
+            public decimal UnitPrice { get; set; }
 
-			//Gyűjtőszámla - szállítólvél kapcsolat
+            //Gyűjtőszámla - szállítólvél kapcsolat
 
-			[ColumnLabel("Szállítólevél sor")]
-			[Description("Kapcsolt szállítólevél sor")]
-			public long? RelDeliveryNoteInvoiceLineID { get; set; }
-
-
-		}
-
-		[ColumnLabel("B/K")]
-		[Description("Bejővő/Kimenő")]
-		public bool Incoming { get; set; }
-
-		[ColumnLabel("Típus")]
-		[Description("Típus")]
-		public string InvoiceType { get; set; }
-
-		[ColumnLabel("Raktár")]
-		[Description("Raktár")]
-		public string WarehouseCode { get; set; }
-
-		[ColumnLabel("Kelt")]
-		[Description("Kiállítás dátuma")]
-		public DateTime InvoiceIssueDate { get; set; }
-
-		[ColumnLabel("Teljesítés")]
-		[Description("Teljesítés dátuma")]
-		public DateTime InvoiceDeliveryDate { get; set; }
-
-		[ColumnLabel("Fiz.hat")]
-		[Description("Fizetési határidő dátuma")]
-		public DateTime PaymentDate { get; set; }
+            [ColumnLabel("Szállítólevél sor")]
+            [Description("Kapcsolt szállítólevél sor")]
+            public long? RelDeliveryNoteInvoiceLineID { get; set; }
 
 
-		[ColumnLabel("Ügyfél ID")]
-		[Description("Ügyfél ID")]
-		public long? CustomerID { get; set; }
+        }
 
-		[ColumnLabel("Kapcsolódó számla")]
-		[Description("Bevételhez kapcsolódó számla")]
-		public string CustomerInvoiceNumber { get; set; }
+        [ColumnLabel("B/K")]
+        [Description("Bejővő/Kimenő")]
+        public bool Incoming { get; set; }
 
-		[ColumnLabel("Fiz.mód")]
-		[Description("Fizetési mód")]
-		public string PaymentMethod { get; set; }
+        [ColumnLabel("Típus")]
+        [Description("Típus")]
+        public string InvoiceType { get; set; }
 
-		[ColumnLabel("Pénznem")]
-		[Description("Pénznem")]
-		public string CurrencyCode { get; set; }
+        [ColumnLabel("Raktár")]
+        [Description("Raktár")]
+        public string WarehouseCode { get; set; }
 
-		[ColumnLabel("Árfolyam")]
-		[Description("Árfolyam")]
-		public decimal ExchangeRate { get; set; }
+        [ColumnLabel("Kelt")]
+        [Description("Kiállítás dátuma")]
+        public DateTime InvoiceIssueDate { get; set; }
 
-		[ColumnLabel("Megjegyzés")]
-		[Description("Megjegyzés")]
-		public string Notice { get; set; }  //AdditionalInvoiceData-ban tároljuk!
+        [ColumnLabel("Teljesítés")]
+        [Description("Teljesítés dátuma")]
+        public DateTime InvoiceDeliveryDate { get; set; }
 
-		[ColumnLabel("Kedvezmény%")]
-		[Description("A számlára adott teljes kedvezmény %")]
-		public decimal InvoiceDiscountPercent { get; set; }
+        [ColumnLabel("Fiz.hat")]
+        [Description("Fizetési határidő dátuma")]
+        public DateTime PaymentDate { get; set; }
 
-		[ColumnLabel("Felhasználó ID")]
-		[Description("Felhasználó ID")]
-		public long? UserID { get; set; } = 0;
+
+        [ColumnLabel("Ügyfél ID")]
+        [Description("Ügyfél ID")]
+        public long? CustomerID { get; set; }
+
+        [ColumnLabel("Kapcsolódó számla")]
+        [Description("Bevételhez kapcsolódó számla")]
+        public string CustomerInvoiceNumber { get; set; }
+
+        [ColumnLabel("Fiz.mód")]
+        [Description("Fizetési mód")]
+        public string PaymentMethod { get; set; }
+
+        [ColumnLabel("Pénznem")]
+        [Description("Pénznem")]
+        public string CurrencyCode { get; set; }
+
+        [ColumnLabel("Árfolyam")]
+        [Description("Árfolyam")]
+        public decimal ExchangeRate { get; set; }
+
+        [ColumnLabel("Megjegyzés")]
+        [Description("Megjegyzés")]
+        public string Notice { get; set; }  //AdditionalInvoiceData-ban tároljuk!
+
+        [ColumnLabel("Kedvezmény%")]
+        [Description("A számlára adott teljes kedvezmény %")]
+        public decimal InvoiceDiscountPercent { get; set; }
+
+        [ColumnLabel("Felhasználó ID")]
+        [Description("Felhasználó ID")]
+        public long? UserID { get; set; } = 0;
 
         [ColumnLabel("Munkaszám")]
         [Description("Munkaszám")]
@@ -129,56 +120,55 @@ namespace bxBE.Application.Commands.cmdInvoice
         [Description("Ár felülvizsgálat jelölése")]
         public bool? PriceReview { get; set; } = false;
 
-		[ColumnLabel("Módosító bizonylat?")]
-		[Description("Módosító bizonylat jelölése")]
-		public bool? Correction { get; set; } = false;
+        [ColumnLabel("Módosító bizonylat?")]
+        [Description("Módosító bizonylat jelölése")]
+        public bool? Correction { get; set; } = false;
 
-		[ColumnLabel("Típus")]
-		[Description("Típus")]
-		public string InvoiceCategory { get; set; } = enInvoiceCategory.NORMAL.ToString();
+        [ColumnLabel("Típus")]
+        [Description("Típus")]
+        public string InvoiceCategory { get; set; } = enInvoiceCategory.NORMAL.ToString();
 
+        [ColumnLabel("Számlasorok")]
+        [Description("Számlasorok")]
+        public List<InvoiceLine> InvoiceLines { get; set; } = new List<InvoiceLine>();
 
-		[ColumnLabel("Számlasorok")]
-		[Description("Számlasorok")]
-		public List<InvoiceLine> InvoiceLines { get; set; } = new List<InvoiceLine>();
+    }
 
-	}
-
-	public class CreateInvoiceCommandHandler : IRequestHandler<CreateInvoiceCommand, Response<Invoice>>
-	{
-		private readonly IInvoiceRepositoryAsync _InvoiceRepository;
-		private readonly IInvoiceLineRepositoryAsync _InvoiceLineRepository;
-		private readonly ICounterRepositoryAsync _CounterRepository;
-		private readonly IWarehouseRepositoryAsync _WarehouseRepository;
-		private readonly ICustomerRepositoryAsync _CustomerRepository;
-		private readonly IProductRepositoryAsync _ProductRepository;
-		private readonly IVatRateRepositoryAsync _VatRateRepository;
+    public class CreateInvoiceCommandHandler : IRequestHandler<CreateInvoiceCommand, Response<Invoice>>
+    {
+        private readonly IInvoiceRepositoryAsync _InvoiceRepository;
+        private readonly IInvoiceLineRepositoryAsync _InvoiceLineRepository;
+        private readonly ICounterRepositoryAsync _CounterRepository;
+        private readonly IWarehouseRepositoryAsync _WarehouseRepository;
+        private readonly ICustomerRepositoryAsync _CustomerRepository;
+        private readonly IProductRepositoryAsync _ProductRepository;
+        private readonly IVatRateRepositoryAsync _VatRateRepository;
         private readonly IExpiringData<ExpiringDataObject> _expiringData;
         private readonly IMapper _mapper;
-		private readonly IConfiguration _configuration;
+        private readonly IConfiguration _configuration;
 
-		public CreateInvoiceCommandHandler(
-			IInvoiceRepositoryAsync InvoiceRepository,
-			IInvoiceLineRepositoryAsync InvoiceLineRepository,
-			ICounterRepositoryAsync CounterRepository,
-			IWarehouseRepositoryAsync WarehouseRepository,
-			ICustomerRepositoryAsync CustomerRepository,
-			IProductRepositoryAsync ProductRepository,
-			IVatRateRepositoryAsync VatRateRepository,
+        public CreateInvoiceCommandHandler(
+            IInvoiceRepositoryAsync InvoiceRepository,
+            IInvoiceLineRepositoryAsync InvoiceLineRepository,
+            ICounterRepositoryAsync CounterRepository,
+            IWarehouseRepositoryAsync WarehouseRepository,
+            ICustomerRepositoryAsync CustomerRepository,
+            IProductRepositoryAsync ProductRepository,
+            IVatRateRepositoryAsync VatRateRepository,
             IExpiringData<ExpiringDataObject> expiringData,
             IMapper mapper, IConfiguration configuration)
-		{
-			_InvoiceRepository = InvoiceRepository;
-			_InvoiceLineRepository = InvoiceLineRepository;
-			_CounterRepository = CounterRepository;
-			_WarehouseRepository = WarehouseRepository;
-			_CustomerRepository = CustomerRepository;
-			_ProductRepository = ProductRepository;
-			_VatRateRepository = VatRateRepository;
+        {
+            _InvoiceRepository = InvoiceRepository;
+            _InvoiceLineRepository = InvoiceLineRepository;
+            _CounterRepository = CounterRepository;
+            _WarehouseRepository = WarehouseRepository;
+            _CustomerRepository = CustomerRepository;
+            _ProductRepository = ProductRepository;
+            _VatRateRepository = VatRateRepository;
             _expiringData = expiringData;
             _mapper = mapper;
-			_configuration = configuration;
-		}
+            _configuration = configuration;
+        }
         /*
 {
   "customerID": 206568,
@@ -224,21 +214,21 @@ namespace bxBE.Application.Commands.cmdInvoice
 
 
         public async Task<Response<Invoice>> Handle(CreateInvoiceCommand request, CancellationToken cancellationToken)
-		{
+        {
 
-			var inv = await bllInvoice.CreateInvoiceAsynch(request, _mapper,
-									_InvoiceRepository,
-									_InvoiceLineRepository,
-									_CounterRepository,
-									_WarehouseRepository,
-									_CustomerRepository,
-									_ProductRepository,
-									_VatRateRepository,
+            var inv = await bllInvoice.CreateInvoiceAsynch(request, _mapper,
+                                    _InvoiceRepository,
+                                    _InvoiceLineRepository,
+                                    _CounterRepository,
+                                    _WarehouseRepository,
+                                    _CustomerRepository,
+                                    _ProductRepository,
+                                    _VatRateRepository,
                                     _expiringData,
                                     cancellationToken);
-			return new Response<Invoice>(inv);
-		}
+            return new Response<Invoice>(inv);
+        }
 
 
-	}
+    }
 }
