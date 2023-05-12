6c8189c2790145e6ce4dbe4fd5396f0a250397be
gyorgyia <antal.gyorgyi@medicover.hu>
2023-05-12 14:52:32 +0200

    WhsTransder lekérdezések

---
+++ bbxBE.Application/Queries/qWhsTransfer/QueryWhsTransferValidator.cs
@@ -0,0 +1,27 @@
+﻿using bbxBE.Application.Interfaces.Repositories;
+using bbxBE.Application.Queries.qWhsTransfer;
+using bbxBE.Common.Consts;
+using bbxBE.Common.Enums;
+using FluentValidation;
+using System;
+
+namespace bbxBE.Application.Commands.cmdInvoice
+{
+
+    public class QueryWhsTransferValidator : AbstractValidator<QueryWhsTransfer>
+    {
+        public QueryWhsTransferValidator(IWhsTransferRepositoryAsync WhsTransferRepositoryAsync)
+        {
+            RuleFor(r => r.WhsTransferStatus)
+            .NotEmpty().WithMessage(bbxBEConsts.ERR_REQUIRED)
+            .Must(CheckWhsTransferStatus).WithMessage(bbxBEConsts.ERR_INVPAYMENTMETHOD);
+
+        }
+        private bool CheckWhsTransferStatus(string whsTransferStatus)
+        {
+            var valid = Enum.TryParse(whsTransferStatus, out enWhsTransferStatus ws);
+            return valid;
+        }
+
+    }
+}
