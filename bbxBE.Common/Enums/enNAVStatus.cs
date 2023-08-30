15cd0f6de3ab76d75dac6199f295268f4b9ea9f8
gyorgyia <antal.gyorgyi@medicover.hu>
2023-08-30 16:36:27 +0200

    NAV DB-k

---
+++ bbxBE.Common/Enums/enNAVStatus.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace bbxBE.Common.Enums
+{
+    public enum enNAVStatus
+    {
+        [Description("Létrehozva")]
+        CREATED,
+        [Description("Elküldött")]
+        SENT,
+        [Description("Rendben")]
+        DONE,
+        [Description("Hiba")]
+        ERROR,
+        [Description("Megszakítva")]
+        ABORTED,
+        [Description("Ismeretlen állapot")]
+        UNKNOWN
+    }
+}
