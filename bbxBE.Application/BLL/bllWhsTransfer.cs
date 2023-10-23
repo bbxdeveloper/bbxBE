fb7f8f2d403365ffd3d52794105736a2a90cd239
gyorgyia <antal.gyorgyi@medicover.hu>
2023-10-23 19:21:52 +0200

    product group + wstransfer refakt

--- bbxBE.Application/BLL/bllWhsTransfer.cs
+++ bbxBE.Application/BLL/bllWhsTransfer.cs
@@ -1,19 +1,12 @@
-﻿using AutoMapper;
-using bbxBE.Application.Commands.cmdWhsTransfer;
+﻿using bbxBE.Application.Commands.cmdWhsTransfer;
 using bbxBE.Application.Interfaces.Repositories;
-using bbxBE.Common;
 using bbxBE.Common.Consts;
-using bbxBE.Common.Enums;
 using bbxBE.Common.Exceptions;
-using bbxBE.Domain.Entities;
-using bxBE.Application.Commands.cmdWhsTransfer;
 using Microsoft.AspNetCore.Mvc;
 using PdfSharp.Pdf;
 using PdfSharp.Pdf.IO;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Xml;
@@ -25,134 +18,6 @@ namespace bbxBE.Application.BLL
 {
     public static class bllWhsTransfer
     {
-        public static async Task<WhsTransfer> CreateWhsTransferAsynch(CreateWhsTransferCommand request,
-                         IMapper mapper,
-                         IWhsTransferRepositoryAsync whsTransferRepository,
-                         IWarehouseRepositoryAsync warehouseRepository,
-                         ICounterRepositoryAsync counterRepository,
-                         IProductRepositoryAsync productRepository,
-                         CancellationToken cancellationToken)
-        {
-            var whsTransfer = mapper.Map<WhsTransfer>(request);
-            var counterCode = "";
-            try
-            {
-
-                await prepareWhsTransferAsynch(whsTransfer, request.FromWarehouseCode, request.ToWarehouseCode, warehouseRepository, productRepository, cancellationToken);
-
-                if (whsTransfer.FromWarehouseID == whsTransfer.ToWarehouseID)
-                {
-                    throw new ResourceNotFoundException(string.Format(bbxBEConsts.ERR_WHSTRANSFERSAMEWHS));
-                }
-
-                whsTransfer.Notice = Utils.TidyHtml(whsTransfer.Notice);
-
-
-                var prefix = "WHT";
-                var whs = whsTransfer.FromWarehouseID.ToString().PadLeft(3, '0');
-                counterCode = String.Format($"{prefix}_{whs}");
-                whsTransfer.WhsTransferNumber = await counterRepository.GetNextValueAsync(counterCode, whsTransfer.FromWarehouseID);
-                whsTransfer.Copies = 1;
-                whsTransfer.WhsTransferStatus = enWhsTransferStatus.READY.ToString();
-
-                await whsTransferRepository.AddWhsTransferAsync(whsTransfer);
-                await counterRepository.FinalizeValueAsync(counterCode, whsTransfer.FromWarehouseID, whsTransfer.WhsTransferNumber);
-                return whsTransfer;
-
-            }
-            catch (Exception)
-            {
-                if (!string.IsNullOrWhiteSpace(whsTransfer.WhsTransferNumber) && !string.IsNullOrWhiteSpace(counterCode))
-                {
-                    await counterRepository.RollbackValueAsync(counterCode, whsTransfer.FromWarehouseID, whsTransfer.WhsTransferNumber);
-                }
-                throw;
-            }
-        }
-
-        public static async Task<WhsTransfer> UpdateWhsTransferAsynch(UpdateWhsTransferCommand request,
-                 IMapper mapper,
-                 IWhsTransferRepositoryAsync whsTransferRepository,
-                 IWarehouseRepositoryAsync warehouseRepository,
-                 ICounterRepositoryAsync counterRepository,
-                 IProductRepositoryAsync productRepository,
-                 CancellationToken cancellationToken)
-        {
-            var whsTransfer = mapper.Map<WhsTransfer>(request);
-            var counterCode = "";
-            try
-            {
-
-                await prepareWhsTransferAsynch(whsTransfer, request.FromWarehouseCode, request.ToWarehouseCode, warehouseRepository, productRepository, cancellationToken);
-                if (whsTransfer.FromWarehouseID == whsTransfer.ToWarehouseID)
-                {
-                    throw new ResourceNotFoundException(string.Format(bbxBEConsts.ERR_WHSTRANSFERSAMEWHS));
-                }
-                whsTransfer.Notice = Utils.TidyHtml(whsTransfer.Notice);
-
-                whsTransfer = await whsTransferRepository.UpdateWhsTransferAsync(whsTransfer);
-                return whsTransfer;
-
-            }
-            catch (Exception)
-            {
-                if (!string.IsNullOrWhiteSpace(whsTransfer.WhsTransferNumber) && !string.IsNullOrWhiteSpace(counterCode))
-                {
-                    await counterRepository.RollbackValueAsync(counterCode, whsTransfer.FromWarehouseID, whsTransfer.WhsTransferNumber);
-                }
-                throw;
-            }
-        }
-
-        private static async Task prepareWhsTransferAsynch(WhsTransfer whsTransfer,
-                        string fromWarehouseCode, string toWarehouseCode,
-                        IWarehouseRepositoryAsync warehouseRepository,
-                        IProductRepositoryAsync productRepository,
-                        CancellationToken cancellationToken)
-        {
-            try
-            {
-
-                var fromWarehouse = await warehouseRepository.GetWarehouseByCodeAsync(fromWarehouseCode);
-                if (fromWarehouse == null)
-                {
-                    throw new ResourceNotFoundException(string.Format(bbxBEConsts.ERR_WAREHOUSENOTFOUND, fromWarehouseCode));
-                }
-                whsTransfer.FromWarehouseID = fromWarehouse.ID;
-
-
-
-                var toWarehouse = await warehouseRepository.GetWarehouseByCodeAsync(toWarehouseCode);
-                if (toWarehouse == null)
-                {
-                    throw new ResourceNotFoundException(string.Format(bbxBEConsts.ERR_WAREHOUSENOTFOUND, toWarehouseCode));
-                }
-                whsTransfer.ToWarehouseID = toWarehouse.ID;
-
-                //Tételsorok előfeldolgozása
-                var lineErrors = new List<string>();
-                foreach (var ln in whsTransfer.WhsTransferLines)
-                {
-                    var rln = whsTransfer.WhsTransferLines.SingleOrDefault(i => i.WhsTransferLineNumber == ln.WhsTransferLineNumber);
-
-                    var prod = productRepository.GetProductByProductCode(rln.ProductCode);
-                    if (prod == null)
-                    {
-                        throw new ResourceNotFoundException(string.Format(bbxBEConsts.ERR_PRODCODENOTFOUND, rln.ProductCode));
-                    }
-
-                    ln.ProductID = prod.ID;
-                    ln.ProductCode = rln.ProductCode;
-                    //ln.Product = prod;
-
-                }
-
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
 
         public static async Task<FileStreamResult> CreateWhsTransferReportAsynch(IWhsTransferRepositoryAsync _whsTransferRepositoryAsync, string reportTRDX, PrintWhsTransferCommand request, CancellationToken cancellationToken)
         {
--- bbxBE.Application/Commands/cmdWhsTransfer/createWhsTransferCommand.cs
+++ bbxBE.Application/Commands/cmdWhsTransfer/createWhsTransferCommand.cs
@@ -1,11 +1,8 @@
-﻿using AutoMapper;
-using bbxBE.Application.BLL;
-using bbxBE.Application.Interfaces.Repositories;
+﻿using bbxBE.Application.Interfaces.Repositories;
 using bbxBE.Application.Wrappers;
 using bbxBE.Common.Attributes;
 using bbxBE.Domain.Entities;
 using MediatR;
-using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -78,31 +75,17 @@ namespace bxBE.Application.Commands.cmdWhsTransfer
     public class CreateWhsTransferCommandHandler : IRequestHandler<CreateWhsTransferCommand, Response<WhsTransfer>>
     {
         private readonly IWhsTransferRepositoryAsync _whsTransferRepositoryAsync;
-        private readonly IWarehouseRepositoryAsync _warehouseRepositoryAsync;
-        private readonly ICounterRepositoryAsync _counterRepository;
-        private readonly IProductRepositoryAsync _productRepository;
-        private readonly IMapper _mapper;
-        private readonly IConfiguration _configuration;
 
         public CreateWhsTransferCommandHandler(
-            IWhsTransferRepositoryAsync whsTransferRepositoryAsync,
-            IWarehouseRepositoryAsync warehouseRepositoryAsync,
-            ICounterRepositoryAsync counterRepository,
-            IProductRepositoryAsync productRepository,
-            IMapper mapper, IConfiguration configuration)
+            IWhsTransferRepositoryAsync whsTransferRepositoryAsync
+            )
         {
             _whsTransferRepositoryAsync = whsTransferRepositoryAsync;
-            _warehouseRepositoryAsync = warehouseRepositoryAsync;
-            _counterRepository = counterRepository;
-            _productRepository = productRepository;
-            _mapper = mapper;
-            _configuration = configuration;
         }
 
         public async Task<Response<WhsTransfer>> Handle(CreateWhsTransferCommand request, CancellationToken cancellationToken)
         {
-            var wh = await bllWhsTransfer.CreateWhsTransferAsynch(request, _mapper,
-                    _whsTransferRepositoryAsync, _warehouseRepositoryAsync, _counterRepository, _productRepository,
+            var wh = await _whsTransferRepositoryAsync.CreateWhsTransferAsynch(request,
                     cancellationToken);
             return new Response<WhsTransfer>(wh);
         }
