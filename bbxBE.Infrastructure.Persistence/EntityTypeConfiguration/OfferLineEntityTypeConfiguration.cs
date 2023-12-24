2980c29d116285c9ff9cbd9d1ec04ebd0579ad9c
gyorgyia <antal.gyorgyi@medicover.hu>
2023-12-24 11:39:21 +0100

    Offer mnetés jav

--- bbxBE.Infrastructure.Persistence/EntityTypeConfiguration/OfferLineEntityTypeConfiguration.cs
+++ bbxBE.Infrastructure.Persistence/EntityTypeConfiguration/OfferLineEntityTypeConfiguration.cs
@@ -5,15 +5,11 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 namespace bbxBE.Infrastructure.Persistence.EntityTypeConfigurations
 {
     //https://stackoverflow.com/questions/51568253/one-to-many-relationship-getting-error-in-entity-framework-core-when-trying-to-i
-    public class OfferEntityTypeConfiguration : IEntityTypeConfiguration<Offer>
+    public class OfferLineEntityTypeConfiguration : IEntityTypeConfiguration<OfferLine>
     {
-        public void Configure(EntityTypeBuilder<Offer> builder)
+        public void Configure(EntityTypeBuilder<OfferLine> builder)
         {
-            builder
-            .HasMany<OfferLine>(g => g.OfferLines)
-            .WithOne(c => c.Offer)
-            .HasForeignKey(s => s.OfferID)
-            .IsRequired(true);
+            //soon
         }
     }
 }
