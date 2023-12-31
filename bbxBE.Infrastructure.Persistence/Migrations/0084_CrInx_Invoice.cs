eb7a16050fea0bd81aea4c4c008b2ebdd0a580f6
gyorgyia <antal.gyorgyi@medicover.hu>
2023-12-31 17:08:49 +0100

    Kiegyenlítetlen számlák szűrőfeltétel bővítés + index

---
+++ bbxBE.Infrastructure.Persistence/Migrations/0084_CrInx_Invoice.cs
@@ -0,0 +1,26 @@
+﻿using FluentMigrator;
+
+//https://code-maze.com/dapper-migrations-fluentmigrator-aspnetcore/
+
+namespace bbxBE.Infrastructure.Persistence.Migrations
+{
+    [Migration(00084, "v00.02.41-INX_InvoiceIncomingPaymentMethodInvoiceIssueDate for unpaid invoices")]
+    public class InitialTables_00084 : Migration
+    {
+        public override void Down()
+        {
+            Delete.Index("INX_InvoiceIncomingPaymentMethodInvoiceIssueDate")
+            .OnTable("Invoice");
+        }
+        public override void Up()
+        {
+
+            Create.Index("INX_InvoiceIncomingPaymentMethodInvoiceIssueDate")
+                         .OnTable("Invoice")
+                         .OnColumn("Incoming").Ascending()
+                         .OnColumn("PaymentMethod").Ascending()
+                         .OnColumn("InvoiceIssueDate").Ascending()
+                         .WithOptions().NonClustered();
+        }
+    }
+}
